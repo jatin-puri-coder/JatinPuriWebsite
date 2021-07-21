510cc324ee549cc59a5efc0c8f60c3682aa1c46b
Jatin-Puri <jatinpuri11@gmail.com>
2021-07-21 12:48:50 +1200

    instal react router dom

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home'
+import { HashRouter as Router, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>Hi! I'm Jatin Puri. Web Developer + Music Artist</h1>
+    </div>
+  );
+}
+
+export default App;
